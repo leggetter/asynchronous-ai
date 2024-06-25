903a8c1628c6fa6b28aefab4f4a3fb8a6b25e8b5
Phil Leggetter <phil@leggetter.co.uk>
2024-06-25 19:32:14 +0100

    chore: update to sync

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import Replicate from "replicate";
 import dotenv from "dotenv";
+import downloadFile from "./utils.js";
 dotenv.config();
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
   userAgent: "https://www.npmjs.com/package/create-replicate",
 });
-// const model =
-//   "stability-ai/sdxl:7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc";
-const model = "stability-ai/stable-diffusion-3";
+const model =
+  "stability-ai/sdxl:7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc";
+// const model = "stability-ai/stable-diffusion-3";
 const input = {
   width: 768,
   height: 768,
   prompt:
-    "An anime style picture of two happy software engineers. One engineer is male and the other is female. They are in Barcelona, Spain, at a technology conference winning a prize. cinematic, dramatic",
+    "An anime style picture of two happy software engineers. They are in Barcelona, Spain, at a technology conference and are winning a prize. cinematic, dramatic",
   refine: "expert_ensemble_refiner",
   scheduler: "K_EULER",
   lora_scale: 0.6,
@@ -28,10 +29,12 @@ const input = {
 
 console.log({ model, input });
 console.log("Running...");
-// const output = await replicate.run(model, { input })
-const output = await replicate.predictions.create({
-  model,
-  webhook: "https://hkdk.events/ca96xo5b2luo7p/replicate",
-  input,
-});
+const output = await replicate.run(model, { input });
+downloadFile(output[0]);
+
+// const output = await replicate.predictions.create({
+//   model,
+//   webhook: "https://hkdk.events/ca96xo5b2luo7p/replicate",
+//   input,
+// });
 console.log("Done!", output);
